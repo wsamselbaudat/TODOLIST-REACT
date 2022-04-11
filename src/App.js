f5109ce9895adba1e10c35eadea2fe83f32622f5
Wiktor Samsel-Baudat <wsamselbaudat@gmail.com>
2022-04-11 19:33:43 +0200

    XD
    XD

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Lista zadań</h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
